8292114824820a0ff539670bb78f373e15568e8d
Kangjunesu <kulbit92@naver.com>
2023-08-30 23:15:11 +0900

    Stream 없이 필터링 출력

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,68 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
-        list.add("Java");
-        list.add("C");
-        list.add("React");
+//        List<String> list1 = new ArrayList<>();
+//        list1.add("Java");
+//        list1.add("C");
+//        list1.add("React");
+//
+//        for(String str : list1){
+//            System.out.println(str);
+//        }
+//
+//        System.out.println("for문없이 출력");
+//        //(파라미터) -> { 실행문 }
+//        //forEach : 리스트가 가지고있는 데이터를 순회
+//        list1.stream().forEach((String str) ->{
+//            System.out.println(str);
+//        });
+//
+//        System.out.println();
+//        System.out.println("한줄로 바꿔서 출력");
+//        // 위를 아래처럼 한줄로  가능 { } 제거
+//        list1.stream().forEach((String str) -> System.out.println(str));
+//        // list가 String 타입이므로 추론이 가능. 파라미터의 타입이 예측 가능하면 생략 가능
+//        list1.stream().forEach(str -> System.out.println(str));
+//        // 이것도 가능
+//        list1.stream().forEach(System.out::println); // 함수 reference 방식이라고 함
 
-        for(String str : list){
-            System.out.println(str);
+        ///////////////////////////////////////////////////
+
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("Song", 45));
+        customers.add(new Customer("Kim", 33));
+        customers.add(new Customer("Park", 21));
+        customers.add(new Customer("Lee", 67));
+        customers.add(new Customer("Choi", 19));
+
+        //1. 나이 30 이상
+        //2. 나이 오름차순
+        //3. 이름 순
+
+        List<Customer> list = new ArrayList<>();
+        for(Customer customer : customers){
+            if(customer.getAge()>30){
+                list.add(customer);
+            }
+        }  // 나이 30 초과 추출
+
+        Collections.sort(list);  // 나이에 대한 오름차순 정렬
+
+        List<String> results = new ArrayList<>();
+        for(Customer customer : list){
+            results.add(customer.getName());
+        }  //나이 30이상, 나이로 오름차순돼있는 이름.
+
+        for(String name : results){
+            System.out.println(name);
         }
-
-        System.out.println("for문없이 출력");
-        //(파라미터) -> { 실행문 }
-        //forEach : 리스트가 가지고있는 데이터를 순회
-        list.stream().forEach((String str) ->{
-            System.out.println(str);
-        });
-
-        System.out.println();
-        System.out.println("한줄로 바꿔서 출력");
-        // 위를 아래처럼 한줄로  가능 { } 제거
-        list.stream().forEach((String str) -> System.out.println(str));
-        // list가 String 타입이므로 추론이 가능. 파라미터의 타입이 예측 가능하면 생략 가능
-        list.stream().forEach(str -> System.out.println(str));
-        // 이것도 가능
-        list.stream().forEach(System.out::println); // 함수 reference 방식이라고 함
-
+        //Kim
+//      Song
+//      Lee
 
 
 
